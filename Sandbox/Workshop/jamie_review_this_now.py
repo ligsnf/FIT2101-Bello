7aed131d686a5281b3f08667ea64ed1e45d758fe
Liangdi Wang <lwan0141@student.monash.edu>
2022-08-02 15:40:36 +1000

    skdskdfnkjds

--- Sandbox/Workshop/jamie_review_this_now.py
+++ Sandbox/Workshop/jamie_review_this_now.py
@@ -1,4 +1,4 @@
 reviewer = "Jamie Nguyen"
 print("skdngsnngjkdsnkjdsnfjkdsfnkjsdfnksdfnsd")
 print(f"hello {reviewer}, review this now please")
-print("skdngsnngjkdsnkjdsnfjkdsfnkjsdfnksdfnsd")
+print("kdnkjdnsfjkdsnfj")
