11c471dd9d8662cbd155f64c0fdb588678689beb
Jiawei Liao <jlia0058@student.monash.edu>
2022-08-30 14:01:01 +1000

    some classes

--- Projects/Bello/assets/js/shared.js
+++ Projects/Bello/assets/js/shared.js
@@ -1,4 +1,21 @@
+// Class to hold all the product backlog items
+class product_backlog {
+    constructor () {
+        this._product_backlog = [];
+    }
+    // Accessors
+    get get_product_backlog() { return this._product_backlog; }
+    add_item(item) {
+        this._product_backlog.push(item);
+    }
+}
+
 // Product Backlog Item Class 
 class PBI {
-    constructor()
+    constructor(name, description, type, tag, num_story_points, status, priority, assignee) {
+        this._name = name;
+        this._description = description;
+        this._type = type;
+        this._tag = tag;
+    }
 }
