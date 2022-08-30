d80fb70ecbdbc216d7aa0871295985b34c04090f
jme-nguyen <104990720+jme-nguyen@users.noreply.github.com>
2022-08-30 14:16:46 +1000

    add functions for local storage

--- Projects/Bello/assets/js/shared.js
+++ Projects/Bello/assets/js/shared.js
@@ -19,3 +19,44 @@ class PBI {
         this._tag = tag;
     }
 }
+
+function checkLSData(key)
+{
+    if (localStorage.getItem(key) != null)
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * retrieveLSData function
+ * Used to retrieve data from LS at a specific key. 
+ * @param {string} key LS Key to be used
+ * @returns data from LS in JS format
+ */
+ function retrieveLSData(key)
+ {
+     let data = localStorage.getItem(key);
+     try
+     {
+         data = JSON.parse(data);
+     }
+     catch(err){}
+     finally
+     {
+         return data;
+     }
+ }
+
+ /**
+ * updateLSData function
+ * Used to store JS data in LS at a specific key
+ * @param {string} key LS key to be used
+ * @param {any} data data to be stored
+ */
+function updateLSData(key, data)
+{
+    let json = JSON.stringify(data);
+    localStorage.setItem(key, json);
+}
