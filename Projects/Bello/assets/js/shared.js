501f888f876561df013f3cc3f930dfe2fbdff198
jme-nguyen <104990720+jme-nguyen@users.noreply.github.com>
2022-08-30 14:24:09 +1000

    add chagnes from main

--- Projects/Bello/assets/js/shared.js
+++ Projects/Bello/assets/js/shared.js
@@ -12,12 +12,35 @@ class product_backlog {
 
 // Product Backlog Item Class 
 class PBI {
-    constructor(name, description, type, tag, num_story_points, status, priority, assignee) {
+    constructor(name, description, type, tag, numStoryPoints, status, priority, assignee) {
         this._name = name;
         this._description = description;
         this._type = type;
         this._tag = tag;
+        this._numStoryPoints = numStoryPoints;
+        this._status = status;
+        this._priority = priority;
+        this._assignee = assignee;
     }
+    // Getters
+    get name() { return this._name; }
+    get description() { return this._description; }
+    get type() {return this._type; }
+    get tag() { return this._tag; }
+    get numStoryPoints() { return this._numStoryPoints; }
+    get status() { return this._status; }
+    get priority() { return this._priority; }
+    get assignee() { return this._assignee; }
+    // Setters
+    set name(newName) { this._name = newName; }
+    set description(newDescription) { this._description = newDescription; }
+    set type(newType) {return this._type = newType; }
+    set tag(newTag) { this._tag = newTag; }
+    set numStoryPoints(newNumStoryPoints) { this._numStoryPoints = newNumStoryPoints; }
+    set status(newStatus) { this._status = newStatus; }
+    set priority(newPriority) { this._priority = newPriority; }
+    set assignee(newAssignee) { this._assignee = newAssignee; }
+
 }
 
 function checkLSData(key)
