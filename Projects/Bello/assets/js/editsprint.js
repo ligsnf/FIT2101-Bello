d9bd9dafde97458f9d7d32f0447962a4a259b89b
jme-nguyen <104990720+jme-nguyen@users.noreply.github.com>
2022-09-20 10:18:37 +1000

    change from header to input field

--- Projects/Bello/assets/js/editsprint.js
+++ Projects/Bello/assets/js/editsprint.js
@@ -9,3 +9,21 @@ function startSprint() {
     updateLSData(SPRINT_INVENTORY_KEY, sprintInventory)
     window.location = "sprintBoard.html"
 }
+
+function editSprint(){
+    let nameHeader = document.getElementById("sprintName");
+    let currentSprintName = nameHeader.innerHTML;
+    nameHeader.remove();
+
+    let nameDiv = document.getElementById("sprintNameDiv");
+    let outputEditName = `<div class="mb-3">
+    <label for="editSprintName" class="form-label">Sprint Name</label>
+    <textarea class="form-control" id="editSprintName" rows="1" placeholder=${currentSprintName}></textarea>
+    </div>`;
+    nameDiv.innerHTML = outputEditName;
+
+    let buttonRef = document.getElementById("buttonDiv");
+    let finishButton = `<button type="button" class="btn btn-primary icon" onclick="saveEdit()">Save</button>`;
+    buttonRef.innerHTML += finishButton;
+
+}
