3bcd4034ef1f3ffd0e9cea61a1498af911c991b3
FIT2099 <zche0202@student.monash.edu>
2022-09-20 14:30:30 +1000

    Solve confict

--- Projects/Bello/assets/js/editsprint.js
+++ Projects/Bello/assets/js/editsprint.js
@@ -10,6 +10,42 @@ function startSprint() {
     window.location = "sprintBoard.html"
 }
 
+function editSprint(){
+    let nameHeader = document.getElementById("sprintName");
+    let currentSprintName = nameHeader.innerHTML;
+    nameHeader.remove();
+
+    let nameDiv = document.getElementById("sprintNameDiv");
+    let outputEditName = `<div class="mb-3">
+    <label for="editSprintName" class="form-label">Sprint Name</label>
+    <textarea class="form-control" id="editSprintName" rows="1" placeholder=${currentSprintName}></textarea>
+    </div>`;
+    nameDiv.innerHTML = outputEditName;
+
+    document.getElementById("editButton").remove()
+
+    let buttonRef = document.getElementById("buttonDiv");
+    let finishButton = `<p>All PBIs; Sprint PBIs</p>
+    <button id="saveButton" type="button" class="btn btn-primary icon" onclick="saveEdit()">Save</button>`;
+    buttonRef.innerHTML = finishButton;
+
+}
+
+function saveEdit(){
+    let newName = document.getElementById("editSprintName").value;
+    sprint.name = newName
+    updateLSData(SPRINT_INVENTORY_KEY,sprintInventory)
+
+    let nameDiv = document.getElementById("sprintNameDiv");
+    outputNewName = `<h1 id="sprintName">${sprint.name}</h1>`;
+    nameDiv.innerHTML = outputNewName
+
+    let resetButtonOutput = `<p>All PBIs; Sprint PBIs</p>
+    <button id="editButton" type="button" class="btn btn-primary icon" onclick="editSprint()">Edit Details</button>
+    <button type="button" class="btn btn-primary icon" onclick="startSprint()">Start Sprint</button>`;
+    document.getElementById("buttonDiv").innerHTML = resetButtonOutput;
+}
+
 function deleteSprint(){
     var sprints = JSON.parse(localStorage.getItem(SPRINT_INVENTORY_KEY));
 
