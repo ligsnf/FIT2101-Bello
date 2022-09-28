a57f876fd1656eda47c4360d87535520e943972c
FIT2099 <zche0202@student.monash.edu>
2022-09-28 21:26:57 +1000

    Shown start and end date when viewing a sprint

--- Projects/Bello/assets/js/currentsprint.js
+++ Projects/Bello/assets/js/currentsprint.js
@@ -4,6 +4,12 @@ let sprint = sprintInventory.inventory[0][index];
 let sprintNameDisplayRef = document.getElementById("sprintName");
 sprintNameDisplayRef.innerHTML = sprint.name;
 
+let sprintStartDateDisplayRef = document.getElementById("startDate");
+sprintStartDateDisplayRef.innerHTML = sprint.startDate;
+
+let sprintEndDateDisplayRef = document.getElementById("endDate");
+sprintEndDateDisplayRef.innerHTML = sprint.endDate;
+
 function completeSprint() {
     sprintInventory.completeSprint(index);
     updateLSData(SPRINT_INVENTORY_KEY, sprintInventory)
